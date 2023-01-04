c6eead9b4b8bfe3e94b987daed9a92b53fa0687a
Kyaw Soe <kyawsh@m1.com.sg>
2023-01-04 21:50:44 +0800

    initial commit with binary search

---
+++ binary_search/target/debug/deps/binary_search-092cc70efc57f714.d
@@ -0,0 +1,5 @@
+/Users/kyawsoehein/Project/Practice_Algorithm/Algorithm/binary_search/target/debug/deps/binary_search-092cc70efc57f714.rmeta: src/main.rs
+
+/Users/kyawsoehein/Project/Practice_Algorithm/Algorithm/binary_search/target/debug/deps/binary_search-092cc70efc57f714.d: src/main.rs
+
+src/main.rs:
---
+++ binary_search/target/debug/deps/binary_search-791f4ac551cfd997.d
@@ -0,0 +1,5 @@
+/Users/kyawsoehein/Project/Practice_Algorithm/Algorithm/binary_search/target/debug/deps/binary_search-791f4ac551cfd997.rmeta: src/main.rs
+
+/Users/kyawsoehein/Project/Practice_Algorithm/Algorithm/binary_search/target/debug/deps/binary_search-791f4ac551cfd997.d: src/main.rs
+
+src/main.rs:
