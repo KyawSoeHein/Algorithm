3e1fa6ab7103d5b4e57521f25bb320675be1ffe6
Kyaw Soe <kyawsh@m1.com.sg>
2023-01-05 23:24:58 +0800

    Working on Binary Search

--- binary_search/src/main.rs
+++ binary_search/src/main.rs
@@ -1,3 +1,37 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let target = ask_target_value();
+    let arr = ask_array_to_search();
+
+    search_for_the_target(target, arr);
+}
+
+fn ask_target_value() -> i32 {
+    println!("Please input value to search greater than 0");
+    let mut target = String::new();
+
+    io::stdin()
+        .read_line(&mut target)
+        .expect("failed to read from stdin");
+
+    let trimmed = target.trim();
+
+    match trimmed.parse::<i32>() {
+        Ok(i) => return i,
+        Err(..) => std::process::exit(1),
+    }
+}
+
+fn ask_array_to_search() -> [i32; 5] {
+    return [22, 10, 9, 7, 3];
+}
+
+fn search_for_the_target(target: i32, arr: [i32; 5]) {
+    let mut max: i8 = arr.len();
+    let 
+
+    arr.sort();
+
+    let medium = arr.len() / 2;
 }
